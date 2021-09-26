00237c44f497817ad17fe7f0df6e15e24475f20f
Lukas Arlauskas <arlauskaslukas@gmail.com>
2021-09-27 01:08:47 +0300

    Basic Hub created

---
+++ API/Hubs/MonstersItemsSpellsHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Hubs
+{
+    public class MonstersItemsSpellsHub : Hub
+    {
+        public async Task CreateNewGame()
+        {
+            Console.WriteLine("got it");
+            //insert generate code logic somehow?
+            int GenCode = 0;
+            //send
+            await Clients.Caller.SendAsync("ReceiveCode", GenCode);
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Hubs;
 
 namespace API
 {
@@ -16,6 +18,7 @@ namespace API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +30,9 @@ namespace API
             }
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<MonstersItemsSpellsHub>("/msihub");
             });
         }
     }
