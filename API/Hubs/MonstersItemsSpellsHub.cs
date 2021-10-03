548731b818366c7413dce50b1ae40c35004db307
arlauskaslukas <arlauskaslukas@gmail.com>
2021-10-03 16:28:40 +0300

    signal r working

--- API/Hubs/MonstersItemsSpellsHub.cs
+++ API/Hubs/MonstersItemsSpellsHub.cs
@@ -8,13 +8,19 @@ namespace API.Hubs
 {
     public class MonstersItemsSpellsHub : Hub
     {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Connected");
+            return base.OnConnectedAsync();
+        }
+
         public async Task CreateNewGame()
         {
-            Console.WriteLine("got it");
+            Console.WriteLine("received");
             //insert generate code logic somehow?
-            int GenCode = 0;
+            int matchId = 450896;
             //send
-            await Clients.Caller.SendAsync("ReceiveCode", GenCode);
+            await Clients.Caller.SendAsync("ReceiveCode", matchId);
         }
     }
 }
